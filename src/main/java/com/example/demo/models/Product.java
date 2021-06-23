48157258a55c70be8ec6d4f2a57f8055ae857f2d
DTikhov <DanT@Zosv.onmicrosoft.com>
2021-06-23 16:39:57 +0300

    hotfix

--- src/main/java/com/example/demo/models/Product.java
+++ src/main/java/com/example/demo/models/Product.java
@@ -13,7 +13,7 @@ import javax.persistence.Table;
 @Table(name = "products")
 public class Product {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(nullable = false)
